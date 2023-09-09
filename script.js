38ef0d91297479db5db878914fd038c900f0be5b
NandoAbdillah <nandoabdilah1606@gmail.com>
2023-09-09 14:29:05 +0700

    versi Jum'at, 8 September 2023
    tugas javascript kelas

--- script.js
+++ script.js
@@ -151,3 +151,159 @@ else if (bulan==12) {
     }
 }
 console.log("Zodiac anda adalah : " + zodiac);
+
+function helloWorld () {
+    alert('Hello JavaScript');
+}
+
+var h1 = document.querySelector("#waktu");
+
+function ubahNama() {
+    var nama = document.querySelector("#inputnama").value;
+    document.querySelector("#namasaya").append(nama + " ");
+}
+
+function waktu() {
+    h1.innerHTML=Date();
+}
+
+function lihatZodiac () {
+    let tanggal=document.querySelector("#tanggal").value;
+    let bulan=document.querySelector("#bulan").value;
+    let zodiac="belum dibuat";
+
+    if (bulan==1) {
+        if(tanggal>0 && tanggal<20) {
+            zodiac='Aquarius';
+        }
+        else {
+            zodiac='Taurus';
+
+        }
+    }
+    else if (bulan==2) {
+        if(tanggal>0 && tanggal<25) {
+            zodiac="Libra";
+        }
+        else {
+            zodiac="Gemini";
+        }
+    }
+    else if (bulan==3) {
+        if(tanggal>0 && tanggal<10) {
+            zodiac ='Gemini';
+        }
+        else {
+            zodiac ='Scorpio';
+        }
+    }
+    else if (bulan==4) {
+        if(tanggal>0 && tanggal<8) {
+            zodiac="Scorpio";
+        }
+        else {
+            zodiac="Virgo";
+        }
+    }
+    else if (bulan==5) {
+        if(tanggal>0 && tanggal<5) {
+            zodiac="Virgo";
+        }
+        else {
+            zodiac="Cancer";
+        }
+    }
+    
+    else if (bulan==6) {
+        if(tanggal>0 && tanggal<2) {
+            zodiac= "Cancer";
+        }
+        else {
+            zodiac= "Sagitarius";
+        }
+    }
+    
+    else if (bulan==7) {
+        if(tanggal>0 && tanggal<25) {
+            zodiac ="Leo";
+        }
+        else {
+            zodiac = "Capicorn"
+        }
+    }
+    
+    else if (bulan==8) {
+        if(tanggal>0 && tanggal<15) {
+            zodiac = "Capicorn";
+        }
+        else {
+            zodiac = "Aries";
+        }
+    }
+    
+    else if (bulan==9) {
+        if(tanggal>0 && tanggal<11) {
+            zodiac="Aries";
+        }
+        else {
+            zodiac="Pisces"
+        } 
+    }
+    
+    else if (bulan==10) {
+        if(tanggal>0 && tanggal<8) {
+            zodiac = "Pisces";
+        }
+    
+        else {
+            zodiac ="Kliwon";
+        }
+    }
+    else if (bulan==11) {
+        if(tanggal>0 && tanggal<5) {
+            zodiac = "Kliwon";
+        }
+    
+        else {
+            zodiac ="Wageh";
+        }
+    }
+    else if (bulan==12) {
+        if(tanggal>0 && tanggal<3) {
+            zodiac = "Wageh";
+        }
+    
+        else {
+            zodiac ="Legi";
+        }
+    }
+
+    document.querySelector("#zodiak").innerHTML=zodiac;
+}
+
+function kali () {
+    let a = document.querySelector("#angka1").value;
+    let b = document.querySelector("#angka2").value;
+    let hasil = a*b;
+    document.querySelector("#hasil").innerHTML=hasil;
+}
+function bagi () {
+    let a = document.querySelector("#angka1").value;
+    let b = document.querySelector("#angka2").value;
+    let hasil = a/b;
+    document.querySelector("#hasil").innerHTML=hasil;
+}
+function kurang () {
+    let a = document.querySelector("#angka1").value;
+    let b = document.querySelector("#angka2").value;
+    let hasil = a-b;
+    document.querySelector("#hasil").innerHTML=hasil;
+}
+function jumlah () {
+    let a = parseInt(document.querySelector("#angka1").value);
+    let b = parseInt(document.querySelector("#angka2").value);
+    a+=b;
+    let hasil = a;
+    document.querySelector("#hasil").innerHTML=hasil;
+}
+
